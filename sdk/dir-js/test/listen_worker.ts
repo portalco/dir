34782f7dca7144a6534086fcc93ed82ff0233714
Árpád Csepi <csepi.arpad@outlook.com>
2025-11-21 01:09:05 +0100

    fix(sdk/js): use correct worker file
    Signed-off-by: Árpád Csepi <csepi.arpad@outlook.com>

--- sdk/dir-js/test/listen_worker.ts
+++ sdk/dir-js/test/listen_worker.ts
@@ -2,20 +2,24 @@ import { workerData } from 'worker_threads';
 import { spawnSync } from 'node:child_process';
 import { env } from 'node:process';
 
-const shell_env = env;
+async function pullRecords() {
+    const shell_env = env;
 
-let commandArgs = ["pull", workerData.recordRef.cid];
+    let commandArgs = ["pull", workerData.recordRef.cid];
 
-if (workerData.spiffeEndpointSocket !== '') {
-    commandArgs.push(...["--spiffe-socket-path", workerData.spiffeEndpointSocket]);
+    if (workerData.spiffeEndpointSocket !== '') {
+        commandArgs.push(...["--spiffe-socket-path", workerData.spiffeEndpointSocket]);
+    }
+
+    for (let count = 0; count < 10; count++) {
+        // Execute command
+        spawnSync(
+            `${workerData.dirctlPath}`, commandArgs,
+            { env: { ...shell_env }, encoding: 'utf8', stdio: 'pipe' },
+        );
+
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
 }
 
-for (let count = 0; count < 10; count++) {
-    // Execute command
-    spawnSync(
-        `${workerData.dirctlPath}`, commandArgs,
-        { env: { ...shell_env }, encoding: 'utf8', stdio: 'pipe' },
-    );
-
-    await new Promise(resolve => setTimeout(resolve, 1000));
-}
+pullRecords()
